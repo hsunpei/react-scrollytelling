73dd5ca5ad467f719e19b92d06c715320593585a
Hsun-Pei Wang <garfieldduck@gmail.com>
2025-09-29 00:24:06 +0200

    🔧 : enhance Tailwind CSS plugin configuration to append existing plugins for improved functionality

--- apps/docs/src/plugins/tailwind-config.js
+++ apps/docs/src/plugins/tailwind-config.js
@@ -2,7 +2,7 @@ module.exports = function tailwindPlugin(context, options) {
   return {
     name: "tailwind-plugin",
     configurePostCss(postcssOptions) {
-      postcssOptions.plugins = [require("@tailwindcss/postcss")];
+      postcssOptions.plugins = [...postcssOptions.plugins,require("@tailwindcss/postcss")];
       return postcssOptions;
     },
   };
