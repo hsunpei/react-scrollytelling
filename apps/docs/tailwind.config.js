b1880e940c7e656d5ac1605aa98554b1609484b1
Hsun-Pei Wang <garfieldduck@gmail.com>
2025-09-28 22:50:25 +0200

    ✨ : add React Scrollytelling packages and Tailwind CSS configuration for enhanced styling

---
+++ apps/docs/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './docs/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './blog/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    '../../node_modules/@react-scrollytelling/layout/**/*.{js,jsx,ts,tsx}',
+    '../../packages/layout/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
